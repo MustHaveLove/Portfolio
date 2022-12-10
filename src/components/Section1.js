aeb84d707feb3ef56a80897c7451598ce4902bd6
MustHaveLove <wodnjs6159@gmail.com>
2022-12-11 05:06:23 +0900

    final

---
+++ src/components/Section1.js
@@ -0,0 +1,14 @@
+import { motion } from "framer-motion";
+import SocialButtons from "./SocialButtons";
+import Title from "./Title";
+
+function Section1() {
+  return (
+    <div className="dark:bg-gray-900 bg-[#ffffff] w-full" id="Section1">
+      <Title />
+    
+    </div>
+  );
+}
+
+export default Section1;
